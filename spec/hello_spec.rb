70a570d66e913cce18b743380dbee137e4cbcd1f
Marcos <marcos.andrade@protonmail.com>
2020-04-21 18:23:11 -0300

    C1 - Hello World de automação com Capybara

---
+++ spec/hello_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+describe 'meu primeiro sript' do
+  it 'visitar a página' do
+    visit 'https://training-wheels-protocol.herokuapp.com/'
+    expect(page.title).to eql 'Training Wheels Protocol'
+  end
+end
