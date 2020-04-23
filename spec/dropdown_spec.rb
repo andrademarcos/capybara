40b4740ff5dc48953379e02dd89846e07f7b7ce6
Marcos <marcos.andrade@protonmail.com>
2020-04-22 22:06:50 -0300

    C5 - Selecionando item em dropdown

---
+++ spec/dropdown_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# :dropdown tag para realizar um únicio teste
+# executando o comando rspec --tag @dropdown
+describe 'Caixa de seleção', :dropdown do
+  it 'item específico simples' do
+    visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+    # select só funciona com dropdown com ID
+    select('Loki', from: 'dropdown')
+  end
+
+  it 'item específico com o find' do
+    visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+    # buscando elemento por classes .avenger-list
+    drop = find('.avenger-list')
+    drop.find('option', text: 'Scott Lang').select_option
+    sleep 3
+  end
+
+  it 'qualquer item', :sample do
+    visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+    drop = find('.avenger-list')
+    # all vai mostrar uma lista de arrays e o sample vai selecionar um item da lista
+    drop.all('option').sample.select_option
+    sleep 3
+  end
+end
