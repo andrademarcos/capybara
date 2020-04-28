51ff1771a213dbbacfb88a9e4448c9528d7002e5
Marcos <marcos.andrade@protonmail.com>
2020-04-28 19:27:08 -0300

    C11 - Aprendendo a usar seletores Angular

---
+++ spec/select2_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+describe 'Select2', :select2 do
+  describe('single', :single) do
+    before(:each) do
+      visit 'https://training-wheels-protocol.herokuapp.com/apps/select2/single.html'
+    end
+
+    it 'seleciona ator por nome' do
+      find('.select2-selection--single').click
+      sleep 1
+      find('.select2-results__option', text: 'Adam Sandler').click
+      sleep 5
+    end
+
+    it 'busca e clica no ator' do
+      find('.select2-selection--single').click
+      sleep 1
+      find('.select2-search__field').set 'Chris Rock'
+      find('.select2-results__option').click
+      sleep 5
+    end
+  end
+
+  def selecione(ator)
+    find('.select2-selection--multiple').click
+    find('.select2-search__field').set ator
+    find('.select2-results__option').click
+  end
+
+  describe('multiple', :multiple) do
+    before(:each) do
+      visit 'https://training-wheels-protocol.herokuapp.com/apps/select2/multi.html'
+    end
+
+    it 'seleciona atores' do
+      atores = ['Jim Carrey', 'Owen Wilson', 'Kevim James']
+      atores.each do |a|
+        selecione(a)
+      end
+    end
+  end
+end
